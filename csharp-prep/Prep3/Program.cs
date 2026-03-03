d4468faadc4731c96da579ffb7997fddb62d160b
Desiring75 <eliasgutierrez919@gmail.com>
2026-03-03 11:26:49 -0500

    Completed Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Step 1: Asking user for a magic number
+        /*Console.Write("What is the magic number? ");
+        string user = Console.ReadLine();
+        int userNumber = int.Parse(user);
+        int userGuess;
+        */
+
+        //Step 3: Creating a random number
+        Random randomGenerator = new Random();
+        int userNumber = randomGenerator.Next(1,100);
+        int userGuess;
+        int tries = 0;
+
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            userGuess = int.Parse(guess);
+
+            if (userGuess == userNumber )
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+            else if (userGuess < userNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+            else
+        {
+            Console.WriteLine("Lower");
+        }
+
+            //Stretch Challenge: Create a tracker to track guesses
+            tries += 1;
+
+        } while (userGuess != userNumber);
+
+        Console.WriteLine($"It took you {tries} guesses.");
+
     }
 }
